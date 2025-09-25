46af6962cdb3d9170b46615c5cde71d7a10f7659
Arowolo22 <emmanuelolalekanarowolo@gmail.com>
2025-09-25 21:12:37 +0100

    Done

--- amesoade-concept/src/app/services/page.jsx
+++ amesoade-concept/src/app/services/page.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image"
 import Navbar from "@/components/Navbar"
 import Link from "next/link";
-import Rubber from "../../assets/Rubber.jpg";
+import Rubber from "../../assets/rubber.jpg";
 import {
   CheckCircleIcon,
   WrenchIcon,
